2701680ffd05c876c9b5c51ca151ba8025319656
John Fry <john@sixheads.com>
2023-12-06 16:29:24 +1100

    feat: Refactor Hero slice and helper components

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,9 +1,30 @@
 import { Content } from '@prismicio/client';
 import { PrismicNextImage, PrismicNextLink } from '@prismicio/next';
 import {
+  JSXMapSerializer,
   PrismicRichText,
   SliceComponentProps,
 } from '@prismicio/react';
+import Bounded from '@/components/Bounded';
+import Button from '@/components/Button';
+import Heading from '@/components/Heading';
+
+const components: JSXMapSerializer = {
+  heading1: ({ children }) => (
+    <Heading
+      as="h1"
+      size="xl"
+      className="md:mb-8 mb-4 mt-12 first:mt-0 last:mb0"
+    >
+      {children}
+    </Heading>
+  ),
+  paragraph: ({ children }) => (
+    <p className="text-2xl text-center font-body leading-10 text-slate-600 mb-4 md:mb-8 max-w-md">
+      {children}
+    </p>
+  ),
+};
 
 /**
  * Props for `Hero`.
@@ -15,46 +36,31 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
   return (
-    <section
-      className="px-4 py-10 md:py-14 md:px-6 lg:py-16"
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <div className="mx-auto w-full max-w-6xl">
-        <div className="grid grid-cols-1 place-items-center text-center">
-          <PrismicRichText
-            field={slice.primary.heading}
-            components={{
-              heading1: ({ children }) => (
-                <h1 className="text-5xl md:text-7xl font-bold leading-tight font-display text-slate-700 tracking-tight">
-                  {children}
-                </h1>
-              ),
-            }}
-          />
-          <PrismicRichText
-            field={slice.primary.body}
-            components={{
-              paragraph: ({ children }) => (
-                <p className="text-2xl text-center font-body leading-10 text-slate-600 mb-4 md:mb-8 max-w-md">
-                  {children}
-                </p>
-              ),
-            }}
-          />
-          <PrismicNextLink
-            field={slice.primary.button_link}
-            className="block w-fit bg-cyan-700 hover:bg-cyan-800 transition-colors duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wider mb-8 md:mb-10"
-          >
-            {slice.primary.button_text}
-          </PrismicNextLink>
-          <PrismicNextImage
-            field={slice.primary.image}
-            className="drop-shadow-xl max-w-4xl w-full"
-          />
-        </div>
+      <div className="grid grid-cols-1 place-items-center text-center">
+        <PrismicRichText
+          field={slice.primary.heading}
+          components={components}
+        />
+        <PrismicRichText
+          field={slice.primary.body}
+          components={components}
+        />
+        <Button
+          field={slice.primary.button_link}
+          className="mb-8 md:mb-10"
+        >
+          {slice.primary.button_text}
+        </Button>
+        <PrismicNextImage
+          field={slice.primary.image}
+          className="drop-shadow-xl max-w-4xl w-full"
+        />
       </div>
-    </section>
+    </Bounded>
   );
 };
 
